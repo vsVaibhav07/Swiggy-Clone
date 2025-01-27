8391805e3f671bfd31bd03557137ee5220dd2f9d
Vaibhav Shukla <154261576+vsVaibhav07@users.noreply.github.com>
2025-01-28 01:08:47 +0530

    Update TopRes.jsx

--- src/componenets/TopRes.jsx
+++ src/componenets/TopRes.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState,useEffect } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import topRes from "../ResChain.json";
-import { SearchContext } from "../contexts/searchContext";
+import { SearchContext } from "../contexts/SearchContext";
 
 const TopRes = () => {
     const [slide, setSlide] = useState(0); // Current slide index
